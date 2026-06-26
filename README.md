f20cb1f1163067b538e4ee379472ebf9942e6129
Diego Ribeiro Wanderley <eliwandejp@gmail.com>
2026-06-26 14:32:11 -0300

    Atualização da ULA da Mic-1 - Etapa 1

---
+++ README.md
@@ -0,0 +1,14 @@
+# ETAPA 1
+## ULA da Mic-1
+
+Para conseguir executar o programa, é importante estar no diretório do projeto e ter dois arquivos:
+
+1. entradas.txt: Arquivo com as entradas para a ULA
+2. logs.txt: Arquivo em branco que será preenchido com os logs
+
+Após isso rode no terminal:
+
+```
+$ gcc projeto.c -o projeto
+$ ./projeto
+```
--- projeto.c
+++ projeto.c
@@ -2,12 +2,20 @@
 #include <stdint.h>
 #include <inttypes.h>
 
-void calcular_ula(int32_t, int32_t, int, int, int, int, int, int, int32_t*, int*);
-void print_binario(int32_t);
+void calcular_ula(int32_t, int32_t, int, int, int, int, int, int, int32_t*, int*, FILE*);
+void print_binario(FILE*, int32_t);
 
 int main() {
     FILE* arq = fopen("entradas.txt", "r");
-    if (!arq) puts("Não foi possível abrir o arquivo");
+    FILE* logs = fopen("logs.txt", "w");
+    if (!arq) {
+        puts("Não foi possível abrir o arquivo");
+        return 0;
+    } 
+    if (!logs) {
+        puts("Não foi possível abrir o arquivo");
+        return 0;
+    } 
 
     int32_t A = 0b11111111111111111111111111111111;
     int32_t B = 0b00000000000000000000000000000001;
@@ -15,8 +23,12 @@ int main() {
     int vai_um = 0;
     int PC = 1;
 
-    printf("A: %" PRId32 "\n", A);
-    printf("B: %" PRId32 "\n", B);
+    fprintf(logs, "A: ", A);
+    print_binario(logs, A);
+    fprintf(logs, "B: ");
+    print_binario(logs, B);
+
+    fprintf(logs, "\nComeço do programa\n");
 
     char bits[7];
     while (fscanf(arq, "%s", bits) == 1) {
@@ -28,16 +40,20 @@ int main() {
         int INVA = bits[4] - '0';
         int INC = bits[5] - '0';
 
-        printf("===============================================================");
-        printf("\nCiclo %d\n", PC);
-        printf("PC = %d\n", PC);
-        printf("IR = %s\n", bits);
-        calcular_ula(A, B, F0, F1, ENA, ENB, INVA, INC, &s, &vai_um);
+        fprintf(logs, "===============================================================");
+        fprintf(logs, "\nCiclo %d\n", PC);
+        fprintf(logs, "\nPC = %d\n", PC);
+        fprintf(logs, "IR = %s\n", bits);
+        calcular_ula(A, B, F0, F1, ENA, ENB, INVA, INC, &s, &vai_um, logs);
         PC++;
     }
+    fprintf(logs, "===============================================================");
+    fprintf(logs, "\nCiclo %d\n", PC);
+    fprintf(logs, "\nPC = %d\n", PC);
+    fprintf(logs, "Fim do arquivo");
 }
 
-void calcular_ula(int32_t A, int32_t B, int F0, int F1, int ENA, int ENB, int INVA, int INC, int32_t* s, int* vai_um) {
+void calcular_ula(int32_t A, int32_t B, int F0, int F1, int ENA, int ENB, int INVA, int INC, int32_t* s, int* vai_um, FILE* logs) {
     int32_t a = ENA ? A : 0; // verifica se ENA é 1 para manter o valor de A
     int32_t b = ENB ? B : 0; // verifica se ENB é 1 para manter o valor de B
     
@@ -70,28 +86,28 @@ void calcular_ula(int32_t A, int32_t B, int F0, int F1, int ENA, int ENB, int IN
 
         /*
         O valor de vai_um é justamente o bit que sobrou da soma_completa, o 33º bit,
-        então deslocamos 32 bits para a direita sa soma_completa, e encontramos nosso vai_um,
+        então deslocamos 32 bits para a direita da soma_completa, e encontramos nosso vai_um,
         o & 1 é justamente por segurança, ele limpa qualquer coisa que estiver acima dele.
         Faz o casting para um inteiro.
         */
         *vai_um = (int)(soma_completa >> 32) & 1;
     }
 
-    printf("B = ");
-    print_binario(b);
-    printf("A = ");
-    print_binario(a);
-    printf("S = ");
-    print_binario(*s);
-    printf("Vai-Um = %d\n", *vai_um);
+    fprintf(logs, "B = ");
+    print_binario(logs, b);
+    fprintf(logs, "A = ");
+    print_binario(logs, a);
+    fprintf(logs, "S = ");
+    print_binario(logs, *s);
+    fprintf(logs, "Vai-Um = %d\n", *vai_um);
 }
 
-void print_binario(int32_t num) {
+void print_binario(FILE* logs, int32_t num) {
     // Faz um loop do bit 31 (mais à esquerda) até o bit 0 (mais à direita)
     for (int i = 31; i >= 0; i--) {
         // Desloca o número para a direita e isola o bit atual
         int bit = (num >> i) & 1;
-        printf("%d", bit);
+        fprintf(logs, "%d", bit);
     }
-    printf("\n");
+    fprintf(logs, "\n");
 }
